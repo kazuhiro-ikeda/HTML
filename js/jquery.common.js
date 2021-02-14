972f9b574e00e4f13e6bcca667a5af97e3bc3959
Kazuhiro Ikeda <info@tamias.co.jp>
2021-02-14 13:22:44 +0900

    footer周りの修正

--- js/jquery.common.js
+++ js/jquery.common.js
@@ -5,14 +5,21 @@ $(function(){
 
 	$('').matchHeight();
 
-	//scroll	
-	$( 'a[href^=#]' ).click(function() {
-	   	var speed = 400; 
-	   	var href= $(this).attr( "href" );
-	   	var target = $(href == "#" || href == "" ? 'html' : href);
-	   	var position = target.offset().top;
-	$( 'body,html' ).animate({scrollTop:position}, speed, 'swing' );
-    	return false;
+	//pagetop
+	var topBtn = $('#btn_top');
+	topBtn.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			topBtn.fadeIn();
+		} else {
+			topBtn.fadeOut();
+		}
+	});
+	topBtn.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		return false;
 	});
 
 	//toggle	
